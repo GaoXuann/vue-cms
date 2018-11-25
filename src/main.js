97f80b41f6b94ef2d0ed1f4805aa6970b0cea4d2
gaoxuan <gaoxuan961217@sina.com>
2018-11-25 21:58:32 +0800

    购物车页面

--- src/main.js
+++ src/main.js
@@ -10,8 +10,38 @@ Vue.use(VueResource)
 
 //导入路由模块
 import router from './router'
-import VueRouter from 'vue-router'
-Vue.use(VueRouter)
+// import VueRouter from 'vue-router'
+// Vue.use(VueRouter)
+
+//导入vuex
+import store from './vuex'
+// import Vue from 'vue'
+// import Vuex from 'vuex'
+// Vue.use(Vuex)
+
+// var store = new Vuex.Store({
+//     state: {
+//         car: []
+//     },
+//     mutations: {
+//         addToCar(state, goodsinfo) {
+//             //假设在car中没有找到相应的数据
+//             var flag = false
+//             state.car.some(item => {
+//                 //在car中找到同样的商品
+//                 if (item.id == goodsinfo.id) {
+//                     item.num += parseInt(goodsinfo.num)
+//                     flag = true
+//                     return true
+//                 }
+//             })
+//             if (!flag) {
+//                 state.car.push(goodsinfo)
+//             }
+//         }
+//     },
+//     getters: {}
+// })
 
 // 设置请求的根路径
 Vue.http.options.root = 'http://www.lovegf.cn:8899/';
@@ -59,5 +89,6 @@ Vue.use(VuePreview)
 new Vue({
     el: '#app',
     render: c => c(app),
-    router
+    router,
+    store
 })
---
+++ src/pages/goodsCommon/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <common :id="id"></common>
+</template>
+
+
+<script>
+import common from '../../components/common.vue'
+export default {
+    data(){
+        return {
+            id:this.$route.params.id
+        }
+    },
+    components:{
+        common
+    }
+}
+</script>
+
+
+<style>
+
+</style>
